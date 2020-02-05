8674a846b3d5a3e630d589762b8c6da7680ea8f6
sebo94 <sebastian.iuga94@gmail.com>
2020-02-05 09:06:06 +0100

    Updates

---
+++ precache-manifest.291a5a925d25f9e1580cc2d3ef49df12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0953d186da3a3634b5a644bdfa655f0",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "10b3190f9b0b7b614b87",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f0ae8086a74a643a259e",
+    "url": "/robofriends/static/css/main.6f581d64.chunk.css"
+  },
+  {
+    "revision": "10b3190f9b0b7b614b87",
+    "url": "/robofriends/static/js/2.4b6b2329.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robofriends/static/js/2.4b6b2329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0ae8086a74a643a259e",
+    "url": "/robofriends/static/js/main.1c9ecccd.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
